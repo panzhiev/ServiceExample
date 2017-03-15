45ff3fb6ead982c012abf955b0ca2acac8d363ce
Panzhiev Timur <panzhiev.timur@gmail.com>
2017-03-15 17:43:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/panzhiev/serviceexample/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.panzhiev.serviceexample;
+
+import android.app.Service;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startService;
+    Service service;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startService = (Button) findViewById(R.id.btn_start_service);
+        startService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                service = new MyService();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/panzhiev/serviceexample/MyService.java
@@ -0,0 +1,51 @@
+package com.example.panzhiev.serviceexample;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Panzhiev on 15.03.2017.
+ */
+
+public class MyService extends Service {
+
+    NotificationManager nm;
+
+    @Override
+    public void onCreate() {
+        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        super.onCreate();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void sendNotification() {
+        final int NOTIFY_ID = 101;
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        Notification.Builder builder = new Notification.Builder(MyService.this);
+        builder.setContentIntent(contentIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle("Service has been started")
+                .setContentText("Follow to Main Activity")
+                .setAutoCancel(false);
+        Notification notification = builder.getNotification();
+        nm.notify(NOTIFY_ID, notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        sendNotification();
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
